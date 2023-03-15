d4127e9ebb635650511b000a8c54499cc3abac4b
w1sp <semen2002bs@gmail.com>
2023-03-15 10:10:47 +0300

    Add project files.

---
+++ PorphumWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PorphumWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
