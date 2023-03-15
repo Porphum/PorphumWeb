b570db78a38ff894d383d75aaa18c22fb95ac018
w1sp <semen2002bs@gmail.com>
2023-03-15 17:22:56 +0300

    #3 add converters

--- PorphumWeb.Logic/Storage/Models/User.cs
+++ PorphumWeb.Logic/Storage/Models/User.cs
@@ -5,6 +5,46 @@
 /// </summary>
 public class User
 {
+    #region Converters
+
+    /// <summary xml:lang="ru">
+    /// Преобразует модель хранилища <see cref="User"/> в доменную <see cref="Logic.Models.User"/>. 
+    /// </summary>
+    /// <param name="storage" xml:lang="ru">Модель хранилища.</param>
+    /// <returns xml:lang="ru">Доменная модель.</returns>
+    /// <exception cref="ArgumentNullException" xml:lang="ru">Если <paramref name="storage"/> - <see langword="null"/>.</exception>
+    public static Logic.Models.User ConvertToDomain(User storage)
+    {
+        ArgumentNullException.ThrowIfNull(storage);
+
+        return new Logic.Models.User(
+            storage.Login,
+            storage.Password,
+            storage.Roles
+                .Select(x => Role.ConvertToDomain(x))
+                .ToHashSet());
+    }
+
+    /// <summary xml:lang="ru">
+    /// Преобразует доменную модель <see cref="Logic.Models.User"/> в модель хранилища <see cref="User"/>. 
+    /// </summary>
+    /// <param name="domain" xml:lang="ru">Доменная модель.</param>
+    /// <returns xml:lang="ru">Модель хранилища.</returns>
+    /// <exception cref="ArgumentNullException" xml:lang="ru">Если <paramref name="domain"/> - <see langword="null"/>.</exception>
+    public static User ConvertToStorage(Logic.Models.User domain)
+    {
+        ArgumentNullException.ThrowIfNull(domain);
+
+        return new User()
+        {
+            Login = domain.Login,
+            Password = domain.Password,
+            Roles = domain.Roles.Select(x => Role.ConvertToStorage(x)).ToHashSet()
+        };
+    }
+
+    #endregion
+
     /// <summary xml:lang="ru">
     /// Идентификатор пользовтеля.
     /// </summary>
