499a47c657e966858b78b289206aeb5985307c2c
w1sp <semen2002bs@gmail.com>
2023-03-15 10:45:25 +0300

    #1 create db

---
+++ DataBases/PorphumWeb/Migrations/1_add_version.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+DROP TABLE IF EXISTS db_version;
+
+CREATE TABLE db_version(
+    version INT NOT NULL,
+	update_date TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+INSERT INTO db_version
+VALUES (1);
+
+COMMIT;
