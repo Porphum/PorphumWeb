154528386cda7993aa9b1acec1fbbde971626656
w1sp <semen2002bs@gmail.com>
2023-07-03 16:20:00 +0300

    #6 add pratial load repository

---
+++ General/Abstractions/Storage/IKeyableRatialLoadRepository{TEntity, TKey}.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Abstractions.Storage;
+
+public interface IKeyableRatialLoadRepository<TEntity, TKey> : IPartialLoadRepository<TEntity>
+{
+    public TEntity? GetPartialByKey(TKey key);
+}
--- General/Abstractions/Storage/IRepository{TEntity}.cs
+++ General/Abstractions/Storage/IRepository{TEntity}.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace General.Storage;
+namespace General.Abstractions.Storage;
 
 /// <summary xml:lang="ru">
 /// Описывает репозиторий сущностей.
