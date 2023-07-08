7c8f2c9ced6e76784ddb8d893942b729288b299f
w1sp <semen2002bs@gmail.com>
2023-07-08 22:20:24 +0300

    #10 add store house repo

---
+++ PorphumSales.Logic/Abstractions/Storage/Repository/IStorehouseRepository.cs
@@ -0,0 +1,16 @@
+﻿using PorphumReferenceBook.Logic.Models.Product;
+using PorphumSales.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorphumSales.Logic.Abstractions.Storage.Repository;
+
+public interface IStorehouseRepository
+{
+    public int GetProductQuantity(Product product);
+
+    public IEnumerable<StorehouseProduct> GetStorehouseProducts(bool isFullLoadedProducts);
+}
---
+++ PorphumSales.Logic/Models/StorehouseProduct.cs
@@ -0,0 +1,22 @@
+﻿using PorphumReferenceBook.Logic.Models.Product;
+using PorphumSales.Logic.Abstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorphumSales.Logic.Models;
+
+public class StorehouseProduct
+{
+    public StorehouseProduct(IMappable<Product> product, int quantity)
+    {
+        Product = product ?? throw new ArgumentNullException();
+        Quantity = quantity;
+    }
+
+    public IMappable<Product> Product { get; }
+
+    public int Quantity { get; }
+}
