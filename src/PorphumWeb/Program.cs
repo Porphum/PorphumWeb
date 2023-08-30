e7041f0c741158f4e1117f0be11b4284fd343ca2
w1sp <semen2002bs@gmail.com>
2023-08-30 23:31:29 +0300

    changes for proxy

--- src/PorphumWeb/Program.cs
+++ src/PorphumWeb/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using PorphumReferenceBook.Logic.Storage;
 using PorphumSales.Logic.Storage;
 using PorphumWeb.Logic.Storage;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+});
 
 builder.Services.AddControllersWithViews();
 
@@ -19,8 +26,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    //app.UseHsts();
 }
+
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
